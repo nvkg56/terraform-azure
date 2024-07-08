1f784a63fa63c15c8beb7239737127f3812e223e
kengon56 <154575385+nvkg56@users.noreply.github.com>
2024-07-08 22:03:13 +0700

    Add files via upload

---
+++ createRG.tf
@@ -0,0 +1,21 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "KG56RG" {
+  name     = "KG56RG"
+  location = "Southeast Asia"
+}
+
